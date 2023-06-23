da82e47d8412622bbf7d00bc5017eaba7070696d
7hainan <thainan239@gmail.com>
2023-06-23 01:13:42 -0300

    add model controllers seeders and migration configurations

---
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email','password');
+
+        if(!Auth::attempt($credentials))
+        {
+            abort(401,'Invalid Credentials');
+        }
+
+        $token = Auth::user()->createToken('jwt');
+
+        return response()->json(['data'=>['token' => $token->plainTextToken ]]);
+
+    
+
+    }
+}
